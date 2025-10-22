466a9d99f61f953ddb9c5db01f89a0de6e2a5429
Benshuai5D <benshuai5d@163.com>
2025-10-22 10:35:00 +0800

    update 17.0.17

--- test/jtreg-ext/requires/VMProps.java
+++ test/jtreg-ext/requires/VMProps.java
@@ -341,6 +341,7 @@ public class VMProps implements Callable<Map<String, String>> {
         vmOptFinalFlag(map, "ClassUnloading");
         vmOptFinalFlag(map, "ClassUnloadingWithConcurrentMark");
         vmOptFinalFlag(map, "UseCompressedOops");
+        vmOptFinalFlag(map, "UseLargePages");
         vmOptFinalFlag(map, "UseVectorizedMismatchIntrinsic");
         vmOptFinalFlag(map, "EnableJVMCI");
         vmOptFinalFlag(map, "EliminateAllocations");
